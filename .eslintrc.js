a0e35c6e9222fadf67b723cf8041f31fc402d319
undancer <undancer@gmail.com>
2020-10-01 20:48:43 +0800

    增加测试和代码质量工具

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
     'extends': [
         'plugin:vue/essential',
         'eslint:recommended',
-        '@vue/typescript/recommended'
+        '@vue/typescript/recommended',
+        '@vue/prettier',
+        '@vue/prettier/@typescript-eslint'
     ],
     parserOptions: {
         ecmaVersion: 2020
@@ -14,5 +16,14 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-    }
+    },
+    overrides: [{
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+      env: {
+        mocha: true
+      }
+    }]
 }
