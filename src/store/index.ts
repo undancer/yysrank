626ec6f12318dfe5240392e563da34471d105189
undancer <undancer@gmail.com>
2020-10-04 01:46:35 +0800

    格式化代码

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,18 @@
 import Vuex from "vuex";
 import Vue from "vue";
-import {history, search} from "@/store/mutations"
-import initialState, {AppState} from "@/store/states";
+import { history, search } from "@/store/mutations";
+import initialState, { AppState } from "@/store/states";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store<AppState>({
-    state: initialState,
-    mutations: {
-        search,
-        history,
-        // RESTORE_MUTATION: vuexLocal.RESTORE_MUTATION // this mutation **MUST** be named "RESTORE_MUTATION"
-    },
-    // plugins: [vuexLocal.plugin]
+  state: initialState,
+  mutations: {
+    search,
+    history
+    // RESTORE_MUTATION: vuexLocal.RESTORE_MUTATION // this mutation **MUST** be named "RESTORE_MUTATION"
+  }
+  // plugins: [vuexLocal.plugin]
 });
-
 
 export default store;
--- src/store/states/AppState.ts
+++ src/store/states/AppState.ts
@@ -1,5 +1,5 @@
 interface AppState {
-    histories: {}[]
+  histories: {}[];
 }
 
 export default AppState;
