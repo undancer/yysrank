f50fbdfdda688159f76a8e8c86d493c506ff97b8
undancer <undancer@gmail.com>
2020-10-03 23:09:03 +0800

    换用一个布局工具

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,44 +1,60 @@
 <template>
-  <a-layout>
-    <a-layout-header class="header" style="padding: 0px">
+  <el-container>
+    <el-header>
       <div class="logo"/>
-      <a-menu
-          :selected-keys="[activeRoute]"
-          :style="{ lineHeight: '64px' }"
+      <el-menu
           mode="horizontal"
-          theme="dark"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          @select="handleSelect"
       >
-        <a-menu-item v-for="(value,name) in routes" :key="'/'+name">
+        <el-menu-item
+            v-for="(value, name, index) in routes"
+            :key="'/' + name"
+            :index="`${index}`"
+        >
           <router-link :to="'/' + name">
             {{ value.name }}
           </router-link>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-header>
-    <a-layout style="padding: 0 24px 24px">
-      <a-page-header
-          :sub-title="subTitle"
-          :title="title"
-      />
-      <!--        <a-page-header-->
-      <!--            :title="title"-->
-      <!--            :sub-title="subTitle"-->
-      <!--            :breadcrumb="{ props: { routes } }"-->
-      <!--        />-->
-
-      <a-layout-content
-          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-      >
-        <router-view/>
-        <UpdateTips/>
-      </a-layout-content>
-    </a-layout>
-  </a-layout>
+        </el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main>
+      <el-row>
+        <el-col>
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+            <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+            <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+          </el-breadcrumb>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col>
+          <router-view/>
+        </el-col>
+      </el-row>
+      <UpdateTips/>
+    </el-main>
+    <!--      <el-aside>-->
+    <!--        aside-->
+    <!--      </el-aside>-->
+    <el-footer>
+      foot
+    </el-footer>
+  </el-container>
 </template>
 
 <script>
 import config from "@/config";
 import UpdateTips from "@/components/UpdateTips";
+import "element-ui/lib/theme-chalk/index.css";
+import Vue from "vue";
+import ElementUI from "element-ui";
+
+Vue.use(ElementUI);
 
 export default {
   name: "Layout",
@@ -59,22 +75,24 @@ export default {
       }
       this.activeRoute = this.$route.fullPath;
     },
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
   },
 
   data() {
     return {
       collapsed: false,
-      title: '',
-      activeRoute: '',
-      subTitle: '',
-      routes: {...config.routes},
+      title: "",
+      activeRoute: "",
+      subTitle: "",
+      routes: {...config.routes}
     };
-  },
-}
+  }
+};
 </script>
 
 <style scoped>
-
 .logo {
   /*width: 120px;*/
   width: 200px;
